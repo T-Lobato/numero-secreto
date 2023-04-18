4ea7f895e2e4ca57c3573e215b84300b0216d516
Thyago Lobato <thyagollobato@gmail.com>
2023-04-18 16:51:42 -0300

    Criando a lógica do jogo

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -11,6 +11,7 @@ recognition.addEventListener('result', onSpeak)
 function onSpeak(e) {
     chute = e.results[0][0].transcript
     exibeChuteNaTela(chute);
+    verificaSeOChutePossuiUmValorValido(chute);
 }
 
 function exibeChuteNaTela(chute) {
@@ -19,3 +20,5 @@ function exibeChuteNaTela(chute) {
     <span class="box">${chute}</span>
     `
 }
+
+recognition.addEventListener('end', () => recognition.start());
--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,5 +1,5 @@
 const menorValor = 0;
-const maiorValor = 10000;
+const maiorValor = 1000;
 const numeroSecreto = gerarNumeroAleatorio();
 
 function gerarNumeroAleatorio() {
